a4425a8c6b5e8b34226f55dfc7c7154890069047
manyxu <manyxu@foxmail.com>
2018-10-29 13:38:36 +0800

    update linuxbuildconfig

--- PHOENIX/linuxbuildconfig.sh
+++ PHOENIX/linuxbuildconfig.sh
@@ -1,11 +1,11 @@
 # install opengl
-sudo apt-get install freeglut3-dev
+apt-get install freeglut3-dev
 
 # cp fmod
-sudo cp -fr ./ThirdPartiesLibs/FMOD /usr/local/lib/
-sudo chmod 777 /usr/local/lib/FMOD
+cp -fr ./ThirdPartiesLibs/FMOD /usr/local/lib/
+chmod 777 /usr/local/lib/FMOD
 
 # for x86
-#sudo cp -f /usr/local/lib/FMOD/linux/lib/x86/libfmod.so.9 /usr/local/lib/
+#cp -f /usr/local/lib/FMOD/linux/lib/x86/libfmod.so.9 /usr/local/lib/
 # for x64
-sudo cp -f /usr/local/lib/FMOD/linux/lib/x86_64/libfmod.so.9 /usr/local/lib/
+cp -f /usr/local/lib/FMOD/linux/lib/x86_64/libfmod.so.9 /usr/local/lib/
