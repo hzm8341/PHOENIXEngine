332305f28678e734ef7928b2589958e5b80eba53
manyxu <manyxu@foxmail.com>
2018-11-01 19:02:21 +0800

    update linuxbuildconfig.sh

--- PHOENIX/linuxbuildconfig.sh
+++ PHOENIX/linuxbuildconfig.sh
@@ -6,6 +6,6 @@ cp -fr ./ThirdPartiesLibs/FMOD /usr/local/lib/
 chmod 777 /usr/local/lib/FMOD
 
 # for x86
-#cp -f /usr/local/lib/FMOD/linux/lib/x86/libfmod.so.9 /usr/local/lib/
+cp -f /usr/local/lib/FMOD/linux/lib/x86/libfmod.so.9 /usr/local/lib/
 # for x64
-cp -f /usr/local/lib/FMOD/linux/lib/x86_64/libfmod.so.9 /usr/local/lib/
+#cp -f /usr/local/lib/FMOD/linux/lib/x86_64/libfmod.so.9 /usr/local/lib/
