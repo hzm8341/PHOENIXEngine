47a47acdf3c0e61322565bc323f67d6c65a9d698
manyxu <manyxu@foxmail.com>
2019-02-14 19:18:42 +0800

    update sh

--- PHOENIX/linuxbuildconfig.sh
+++ PHOENIX/linuxbuildconfig.sh
@@ -5,7 +5,8 @@ apt-get install freeglut3-dev
 cp -fr ./ThirdPartiesLibs/FMOD /usr/local/lib/
 chmod 777 /usr/local/lib/FMOD
 
-# for x86
-cp -f /usr/local/lib/FMOD/linux/lib/x86/libfmod.so.9 /usr/local/lib/
-# for x64
-#cp -f /usr/local/lib/FMOD/linux/lib/x86_64/libfmod.so.9 /usr/local/lib/
+if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ] ; then
+    cp -f /usr/local/lib/FMOD/linux/lib/x86_64/libfmod.so.9 /usr/local/lib/
+else
+    cp -f /usr/local/lib/FMOD/linux/lib/x86/libfmod.so.9 /usr/local/lib/
+fi
