7112c13a92d3f654bfd12396ea75774fe2f007e2
manyxu <realmany@qq.com>
2019-02-22 16:14:23 +0800

    update easyPR

--- PHOENIX/Projects/Client/EasyPR/EasyPR/src/core/plate_judge.cpp
+++ PHOENIX/Projects/Client/EasyPR/EasyPR/src/core/plate_judge.cpp
@@ -15,7 +15,7 @@ namespace easypr {
   }
 
   PlateJudge::PlateJudge() { 
-    svm_ = ml::SVM::load<ml::SVM>(kDefaultSvmPath); 
+    svm_ = ml::SVM::load(kDefaultSvmPath); 
     //svm_ = ml::SVM::load<ml::SVM>(kLBPSvmPath);   
     extractFeature = getLBPFeatures;
   }
@@ -26,7 +26,7 @@ namespace easypr {
       if (!svm_->empty())
         svm_->clear();
 
-      svm_ = ml::SVM::load<ml::SVM>(path);
+      svm_ = ml::SVM::load(path);
     }
   }
 
