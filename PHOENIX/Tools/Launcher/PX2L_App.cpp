c709937357eb1ed40575280d11b9f281e4794c5b
manyxu <manyxu@foxmail.com>
2019-02-24 10:26:03 +0800

    update size

--- PHOENIX/Tools/Launcher/PX2L_App.cpp
+++ PHOENIX/Tools/Launcher/PX2L_App.cpp
@@ -294,7 +294,7 @@ L_Frame *L_App::CreateMainFrame(const std::string &name)
 #endif
 
 	RenderWindow *rw = PX2_GR.GetMainWindow();
-	rw->SetScreenSize(Sizef(440.0, 640.0f));
+	rw->SetScreenSize(Sizef(440.0, 680.0f));
 	rw->SetTitle(title);
 
 	// load dll
