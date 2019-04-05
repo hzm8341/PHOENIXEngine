e9e8a8632a840eb6b05a8d3a6fabcd691de3f76e
manyxu <realmany@qq.com>
2019-04-05 21:13:48 +0800

    update script

--- Bin/Data/engine/scripts/lua/agent/ActorControllerPath.lua
+++ Bin/Data/engine/scripts/lua/agent/ActorControllerPath.lua
@@ -22,31 +22,31 @@ function ActorControllerPath:OnAttached()
 end
 
 function ActorControllerPath:OnInitUpdate()
-	if 1 == self._direction then
-		local agentPath = AIAgentPath()
-		self._path0 = agentPath
-		agentPath:AddPoint(APoint(0.0, 0.0, 0.0))
-		agentPath:AddPoint(APoint(0.0, 2.0, 0.0))
-		agentPath:AddPoint(APoint(1.0, 2.0, 0.0))
-		agentPath:ConfigPoints(0.05, false)
-		self._agent:SetPath(agentPath)
-		self._curPath = 0
+	-- if 1 == self._direction then
+	-- 	local agentPath = AIAgentPath()
+	-- 	self._path0 = agentPath
+	-- 	agentPath:AddPoint(APoint(0.0, 0.0, 0.0))
+	-- 	agentPath:AddPoint(APoint(0.0, 2.0, 0.0))
+	-- 	agentPath:AddPoint(APoint(1.0, 2.0, 0.0))
+	-- 	agentPath:ConfigPoints(0.05, false)
+	-- 	self._agent:SetPath(agentPath)
+	-- 	self._curPath = 0
 
-		local agentPath1 = AIAgentPath()
-		agentPath1:AddPoint(APoint(1.0, 2.0, 0.0))
-		agentPath1:AddPoint(APoint(0.0, 2.0, 0.0))
-		agentPath1:AddPoint(APoint(0.0, 0.0, 0.0))
-		agentPath1:ConfigPoints(0.05, false)
-		self._path1 = agentPath1
-	elseif 2 == self._direction then
-		local agentPath = AIAgentPath()
-		agentPath:AddPoint(APoint(0.0, 10.0, 0.0))
-		agentPath:AddPoint(APoint(10.0, 10.0, 0.0))
-		agentPath:AddPoint(APoint(10.0, 0.0, 0.0))
-		agentPath:AddPoint(APoint(0.0, 0.0, 0.0))
-		agentPath:ConfigPoints(0.05, true)
-		self._agent:SetPath(agentPath)
-	end
+	-- 	local agentPath1 = AIAgentPath()
+	-- 	agentPath1:AddPoint(APoint(1.0, 2.0, 0.0))
+	-- 	agentPath1:AddPoint(APoint(0.0, 2.0, 0.0))
+	-- 	agentPath1:AddPoint(APoint(0.0, 0.0, 0.0))
+	-- 	agentPath1:ConfigPoints(0.05, false)
+	-- 	self._path1 = agentPath1
+	-- elseif 2 == self._direction then
+	-- 	local agentPath = AIAgentPath()
+	-- 	agentPath:AddPoint(APoint(0.0, 10.0, 0.0))
+	-- 	agentPath:AddPoint(APoint(10.0, 10.0, 0.0))
+	-- 	agentPath:AddPoint(APoint(10.0, 0.0, 0.0))
+	-- 	agentPath:AddPoint(APoint(0.0, 0.0, 0.0))
+	-- 	agentPath:ConfigPoints(0.05, true)
+	-- 	self._agent:SetPath(agentPath)
+	-- end
 end
 
 function ActorControllerPath:OnPUpdate()
@@ -55,70 +55,6 @@ function ActorControllerPath:OnPUpdate()
 	
 	-- self
     local position = self._agent:GetPosition()
-	local radius = self._agent:GetRadius()
-    
-	-- follow path
-	local followForce = self._agent:ForceToFollowPath(3.0)
-	followForce:Normalize()
-	local followForce1 = followForce:Dot(1.0)
+	local radius = self._agent:GetRadius()  
 
-	local stayForce = self._agent:ForceToStayOnPath(3.0)
-	stayForce:Normalize()
-	-- local wanderForce = self._agent:ForceToWander(elapsedSeconds*1000.0)
-	-- wanderForce:Normalize()
-	
-	local followForceAll0 = followForce1:Add(stayForce:Dot(1.0))
-	--local followForceAll1 = followForceAll0:Add(wanderForce:Dot(0.25))
-	local followForceAll1 = followForceAll0
-	
-	-- avoid
-	local avoidAgentForce = self._agent:ForceToAvoidAgents(3)
-    local avoidObjectForce = self._agent:ForceToAvoidObjects(3)
-	local avoidanceMultiplier = 2
-	local agentForce = avoidAgentForce:Dot(avoidanceMultiplier)
-	local objForce = avoidObjectForce:Dot(avoidanceMultiplier)
-	
-	-- all force
-	local steeringForces = followForceAll1
-	local force0 = followForceAll1:Add(agentForce)
-	steeringForces = force0:Add(objForce)
-
-	local isPathOver0 = true
-	local isPathOver1 = true
-
-	if 0 == self._curPath then
-	 	isPathOver0 = self._path0:IsPathOver(position)
-	 	if isPathOver0 then
-	 		self._agent:SetPath(self._path1)
-	 		self._curPath = 1
-	 	end
-	end
-	if 1 == self._curPath then
-	 	isPathOver1 = self._path1:IsPathOver(position)
-	 	if isPathOver1 then
-	 		self._agent:SetPath(self._path0)
-	 		self._curPath = 0
-	 	end
-	end
-
-	if 0 == self._curPath and not isPathOver0 then
-		local targetSpeed = 0.2;
-		-- Accelerate pathing agents to a minimum speed.
-		if (self._agent:GetSpeed() < targetSpeed) then
-			--local speedForce = self._agent:ForceToTargetSpeed(targetSpeed)
-			--steeringForces = steeringForces:Add(speedForce)
-		end
-	end
-		
-	if 1 == self._curPath and not isPathOver1 then
-	 	local targetSpeed = 0.2;
-	 	-- Accelerate pathing agents to a minimum speed.
-	 	if (self._agent:GetSpeed() < targetSpeed) then
-	 		--local speedForce = self._agent:ForceToTargetSpeed(targetSpeed)
-	 		--steeringForces = steeringForces:Add(speedForce)
-	 	end
-	end
-	
-	Agent_ApplyPhysicsSteeringForce(self._agent, steeringForces, elapsedSeconds)
-	Agent_ClampHorizontalSpeed(self._agent)
 end
